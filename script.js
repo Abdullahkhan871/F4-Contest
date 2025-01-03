e6e51aae78638a18257a43f5a043c3f9b81cd12a
Abdullahkhan871 <123239280+Abdullahkhan871@users.noreply.github.com>
2025-01-04 01:53:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let form = document.querySelector("form");
+let tbody = document.querySelector("tbody");
 let userInputs = document.querySelectorAll("form input");
 
 form.addEventListener("submit", handleUserData);
@@ -8,6 +9,7 @@ let count = 0;
 
 function handleUserData(e) {
   e.preventDefault();
+  let emptyInput = false;
   let userData = {};
   userData["ID"] = ++count;
   userInputs.forEach((item) => {
@@ -15,12 +17,27 @@ function handleUserData(e) {
       userData[item.name] = item.value;
     } else {
       item.style.border = "1px solid red";
+      emptyInput = true;
     }
     item.value = "";
   });
+
+  if (emptyInput) return;
   arrayOfStudents.push(userData);
+  addTableRow(userData);
 }
 
+function addTableRow(userData) {
+  let tr = document.createElement("tr");
+  for (const element in userData) {
+    let td = document.createElement("td");
+    td.innerHTML = userData[element];
+    tr.append(td);
+  }
+  tbody.append(tr);
+}
+
+// when user Input not empty
 userInputs.forEach((item) => {
   item.addEventListener("input", () => {
     if (item.value != "") {
